dc09f0474aa68304faaa63072bb3a8c4844b3a70
Jacob Barry <the.jacobbarry@gmail.com>
2018-11-12 16:12:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 
-Def board = [" "," "," "," "," "," "," "," "," "]
+puts board = [" "," "," "," "," "," "," "," "," "]
 
 # out the current state.
 # Would Print:
